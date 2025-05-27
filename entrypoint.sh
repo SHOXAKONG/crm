d922274d3e488cbce677ca685579de3017354944
SHOXAKONG <bekmurodovshohruh0224@egmail.com>
2025-05-27 21:12:43 +0500

    Add

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
-# Apply migrations
-echo "Running migrations..."
-python manage.py migrate
+echo "Waiting for PostgreSQL..."
 
-# Start Gunicorn
-exec "$@"
+while ! nc -z $DB_HOST $DB_PORT; do
+  sleep 0.5
+done
+
+echo "PostgreSQL started"
+
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+gunicorn djcrm.wsgi:application --bind 0.0.0.0:8080
