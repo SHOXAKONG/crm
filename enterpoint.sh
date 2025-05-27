58462ede0ce856a140843828696bddc351a247b2
SHOXAKONG <bekmurodovshohruh0224@egmail.com>
2025-05-27 13:28:01 +0500

    Add

---
+++ enterpoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Apply migrations
+echo "Running migrations..."
+python manage.py migrate --noinput
+
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+# Start Gunicorn
+exec "$@"
